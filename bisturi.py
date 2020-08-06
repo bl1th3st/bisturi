c084df575b6ace7c2d3b30de9c38b9f93e78b703
blithest <blithest@protonmail.com>
2020-08-06 00:47:34 -0300

    starting to handle ip address

--- bisturi.py
+++ bisturi.py
@@ -1,4 +1,11 @@
 from subprocess import call
+from ipaddress import ip_network
+
+ip_range = input("Ip range: ")
+
+hosts = [x for x in ip_network(ip_range).hosts()]
+
+print(hosts)
 
 out_buffer = None
 value = call(['nmap'], stdout=out_buffer)
